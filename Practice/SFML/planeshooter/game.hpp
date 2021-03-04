af1860d3602cba5595331898c39eadfcf700645e
jonhogan <jon.hogan83@gmail.com>
2021-03-03 22:27:33 -0600

    Pushed from Tenjin

---
+++ Practice/SFML/planeshooter/game.hpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+class GAME
+{
+    protected:
+        sf::RenderWindow mWindow;
+        sf::CircleShape mPlayer;
+    
+    public:
+
+        GAME();
+        void Run();
+        void ProcessEvents();
+        void Update();
+        void Render();
+
+};
