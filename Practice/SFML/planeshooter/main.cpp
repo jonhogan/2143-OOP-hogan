00ad83800cce92cae579d8a6a840f69617ce6723
jonhogan <jon.hogan83@gmail.com>
2021-03-07 12:58:15 -0600

    Pushed from Tenjin

--- Practice/SFML/planeshooter/main.cpp
+++ Practice/SFML/planeshooter/main.cpp
@@ -1,3 +1,14 @@
+/*************************************************************
+*                                                            *
+*   Author  : Jonathan Hogan                                 *
+*   E-mail  : jon.hogan83@gmail.com                          *
+*   Program : P01                                            *
+*   Course  : OOP - 2143                                     *
+*   Semester: Spring 2021                                    *
+*                                                            *
+*************************************************************/
+
+
 #include <SFML/Graphics.hpp>
 #include "game.hpp"
 
