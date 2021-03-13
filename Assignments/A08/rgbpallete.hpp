6e565013d64057662a17fac36ed5bcaedde67946
jonhogan <jon.hogan83@gmail.com>
2021-03-13 11:50:31 -0600

    Pushed from Tenjin

--- Assignments/A08/rgbpallete.hpp
+++ Assignments/A08/rgbpallete.hpp
@@ -1,29 +1,37 @@
 #include <iostream>
 
+
+
 /**
  * This class is a simple bare bones example showing use of a copy constructor
  * for the question on the exam.
  * 
  */
-class NumContainer{
-    int *array;     // int pointer (dynamic memory)
-    int size;       
-public:
-    NumContainer(){
-        size = 100;
-        array = new int[size];
-    }
-    NumContainer(int s){
-        size = s;
-        array = new int[size];
-    }
-    NumContainer(const NumContainer& other){
-        size = other.size;
-        array = new int[size];
-        for(int i=0;i<size;i++){
-            array[i] = other.array[i];
+class RGBPallette
+{
+    protected:
+        RgbColor *array;     // int pointer (dynamic memory)
+        int size;       
+    public:
+        RGBPallette()
+        {
+            size = 100;
+            array = new RgbColor[size];
         }
-    }
+        RGBPallette(int s)
+        {
+            size = s;
+            array = new RgbColor[size];
+        }
+        RGBPallette(const RGBPallette& other)
+        {
+            size = other.size;
+            array = new RgbColor[size];
+            for(int i=0;i<size;i++)
+            {
+                array[i] = other.array[i];
+            }
+        }
 };
 
 
@@ -31,30 +39,33 @@ public:
  * This is an implementation of our single color rgb class. 
  * 
  */
-class RgbColor{
-    int r;                  //int 0-255 red
-    int g;                  //int 0-255 green
-    int b;                  //int 0-255 blue
-public: 
-    RgbColor();             // default constructor
-    RgbColor(int);          // overloaded 1
-    RgbColor(int,int,int);  // overloaded 2
+class RgbColor
+{
+    protected:
+        int r;                  //int 0-255 red
+        int g;                  //int 0-255 green
+        int b;                  //int 0-255 blue
 
-    void SetR(int);         // setter red
-    void SetG(int);         // setter green
-    void SetB(int);         // setter blue
+    public: 
+        RgbColor();             // default constructor
+        RgbColor(int);          // overloaded 1
+        RgbColor(int,int,int);  // overloaded 2
 
-    int GetR();             // getter red
-    int GetG();             // getter green
-    int GetB();             // getter blue
+        void SetR(int);         // setter red
+        void SetG(int);         // setter green
+        void SetB(int);         // setter blue
 
-    void GrayScale();       // averages colors
+        int GetR();             // getter red
+        int GetG();             // getter green
+        int GetB();             // getter blue
 
-    // print to stdout
-    friend std::ostream& operator<<(std::ostream&,const RgbColor&);
+        void GrayScale();       // averages colors
 
-    // add (mix) two colors
-    RgbColor operator+(const RgbColor& );
+        // print to stdout
+        friend std::ostream& operator<<(std::ostream&,const RgbColor&);
+
+        // add (mix) two colors
+        RgbColor operator+(const RgbColor& );
 
 };
 
