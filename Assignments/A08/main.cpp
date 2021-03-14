83e0675ea598e3875a937b605c870a562da25e4f
jonhogan <jon.hogan83@gmail.com>
2021-03-14 12:21:14 -0500

    Pushed from Tenjin

--- Assignments/A08/main.cpp
+++ Assignments/A08/main.cpp
@@ -3,8 +3,15 @@
 
 int main()
 {
-    RgbColor Color1(255,200,11);
-    RgbColor Color2(100,14,123);
-    RgbColor Color3 = Color1 + Color2;
-    std::cout<<Color3<<std::endl;
+    RGBColor Color1(255,200,11);
+    RGBColor Color2(100,14,123);
+    RGBColor Color3 = Color1 + Color2;
+    RGBColor Color4;
+    std::cout << "Color 4: " << Color4 << std::endl;
+    Color4 = Color3;
+    std::cout << "Color 3: " << Color3 << std::endl;
+    std::cout << "Color 4: " << Color4 << std::endl;
+    Color3.SetR(230);
+    std::cout << "Color 3: " << Color3 << std::endl;
+    std::cout << "Color 4: " << Color4 << std::endl;
 }
--- Assignments/A08/rgbpallete.hpp
+++ Assignments/A08/rgbpallete.hpp
@@ -3,43 +3,11 @@
 
 
 /**
- * This class is a simple bare bones example showing use of a copy constructor
- * for the question on the exam.
- * 
- */
-class RGBPallette
-{
-    protected:
-        RgbColor *array;     // int pointer (dynamic memory)
-        int size;       
-    public:
-        RGBPallette()
-        {
-            size = 100;
-            array = new RgbColor[size];
-        }
-        RGBPallette(int s)
-        {
-            size = s;
-            array = new RgbColor[size];
-        }
-        RGBPallette(const RGBPallette& other)
-        {
-            size = other.size;
-            array = new RgbColor[size];
-            for(int i=0;i<size;i++)
-            {
-                array[i] = other.array[i];
-            }
-        }
-};
+* This is an implementation of our single color rgb class. 
+* 
+*/
 
-
-/**
- * This is an implementation of our single color rgb class. 
- * 
- */
-class RgbColor
+class RGBColor
 {
     protected:
         int r;                  //int 0-255 red
@@ -47,51 +15,116 @@ class RgbColor
         int b;                  //int 0-255 blue
 
     public: 
-        RgbColor();             // default constructor
-        RgbColor(int);          // overloaded 1
-        RgbColor(int,int,int);  // overloaded 2
+        RGBColor()             // default constructor
+        {
+            r = g = b = 0;
+        }
 
-        void SetR(int);         // setter red
-        void SetG(int);         // setter green
-        void SetB(int);         // setter blue
+        RGBColor(int color)           // overloaded 1
+        {
+            r = g = b = color;
+        }
 
-        int GetR();             // getter red
-        int GetG();             // getter green
-        int GetB();             // getter blue
+        RGBColor(int red, int green, int blue)   // overloaded 2
+        {
+            r = red;
+            g = green;
+            b = blue;
+        }
 
-        void GrayScale();       // averages colors
+        void SetR(int _r)       // setter red
+        {
+            r = _r;
+        }
+
+        void SetG(int _g)       // setter green
+        {
+            g =_g;
+        }
+
+        void SetB(int _b)       // setter blue
+        {
+            b = _b;
+        }
+
+        int GetR()              // getter red
+        {
+            return r;
+        }
+
+        int GetG()              // getter green
+        {
+            return g;
+        }
+
+        int GetB()              // getter blue
+        {
+            return b;
+        }
+
+        void GrayScale()        // averages colors
+        {
+            int gray;
+            gray = (r + g + b)/3;
+            r = g = b = gray;
+        }
 
         // print to stdout
-        friend std::ostream& operator<<(std::ostream&,const RgbColor&);
+        friend std::ostream& operator<<(std::ostream& os,const RGBColor& rhs)
+        {
+            os<<"["<<rhs.r<<","<<rhs.g<<","<<rhs.b<<"]";
+            return os;     
+        }
 
         // add (mix) two colors
-        RgbColor operator+(const RgbColor& );
+        RGBColor operator+(const RGBColor& rhs)
+        {
+            int r = (this->r + rhs.r) / 2;
+            int g = (this->g + rhs.g) / 2;
+            int b = (this->b + rhs.b) / 2;
+            
+            return *this;
+        }
+
+        RGBColor operator=(const RGBColor& rhs)
+        {
+            this -> r = rhs.r;
+            this -> g = rhs.g;
+            this -> b = rhs.b;
+
+            return *this;
+        }
 
 };
 
-RgbColor RgbColor::operator+(const RgbColor& rhs){
-    int r = (this->r + rhs.r) / 2;
-    int g = (this->g + rhs.g) / 2;
-    int b = (this->b + rhs.b) / 2;
-
-    return *this;
-}
-
-RgbColor::RgbColor(){
-    r = g = b = 0;
-}
-
-RgbColor::RgbColor(int color){
-    r = g = b = color;
-}
-
-RgbColor::RgbColor(int _r,int _g, int _b){
-    r = _r;
-    g = _g;
-    b = _b;
-}
-
-std::ostream& operator<<(std::ostream& os,const RgbColor& rhs){
-    os<<"["<<rhs.r<<","<<rhs.g<<","<<rhs.b<<"]";
-    return os;
-}
+/**
+ * This class is a simple bare bones example showing use of a copy constructor
+ * for the question on the exam.
+ * 
+ */
+class RGBPallette
+{
+    protected:
+        RGBColor *array;     // int pointer (dynamic memory)
+        int size;       
+    public:
+        RGBPallette()
+        {
+            size = 100;
+            array = new RGBColor[size];
+        }
+        RGBPallette(int s)
+        {
+            size = s;
+            array = new RGBColor[size];
+        }
+        RGBPallette(const RGBPallette& other)
+        {
+            size = other.size;
+            array = new RGBColor[size];
+            for(int i=0;i<size;i++)
+            {
+                array[i] = other.array[i];
+            }
+        }
+};
